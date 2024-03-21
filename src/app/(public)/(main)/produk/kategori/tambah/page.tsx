05114e579174e2a934113d95dbb404de19a1e1f4
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-03-21 13:00:27 +0700

    Feat : Ganti Form Kategori

--- src/app/(public)/(main)/produk/kategori/tambah/page.tsx
+++ src/app/(public)/(main)/produk/kategori/tambah/page.tsx
@@ -5,7 +5,7 @@ const Form = dynamic(()=> import("@/app/components/product/kategori/form"))
 const TambahKategori = () => {
     return (
         <main className="bg-white p-14">
-            <h1 className="text-gray-900 font-semibold text-xl">Tambah Produk</h1>
+            <h1 className="text-gray-900 font-semibold text-xl">Tambah Kategori</h1>
             <h1 className="text-gray-400 text-sm">Label pada kotak yang ditandai dengan * adalah Wajib di input.</h1>
             <Form />
         </main>
--- src/app/components/product/kategori/form.tsx
+++ src/app/components/product/kategori/form.tsx
@@ -1,18 +1,18 @@
 const Form = () => {
     return (
         <form className="mt-10">
-            <h1 className="py-2">Foto Produk*</h1>
+            <h1 className="py-2">Foto*</h1>
             <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
             <div className="grid lg:grid-cols-2 gap-5 mt-5">
                 <label className="form-control w-full max-w-xs">
                     <div className="label">
-                        <span className="label-text">Nama Produk*(Wajib)</span>
+                        <span className="label-text">Nama Kategori*(Wajib)</span>
                     </div>
-                    <input type="text" placeholder="Nama Produk" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Nama Kategori" className="input input-bordered w-full max-w-xs" />
                 </label>
                 <label className="form-control w-full max-w-xs">
                     <div className="label">
-                        <span className="label-text">Parent Produk*(Wajib)</span>
+                        <span className="label-text">Parent Kategori*(Wajib)</span>
                     </div>
                     <select id="countries" className="bg-gray-50 border input input-bordered w-full max-w-xs">
                         <option selected className="text-gray-200">Pilih</option>
@@ -21,18 +21,6 @@ const Form = () => {
                         <option value="">France</option>
                         <option value="">Germany</option>
                     </select>
-                </label>
-                <label className="form-control w-full max-w-xs">
-                    <div className="label">
-                        <span className="label-text">Quantity Produk*(Wajib)</span>
-                    </div>
-                    <input type="text" placeholder="Quantity" className="input input-bordered w-full max-w-xs" />
-                </label>
-                <label className="form-control w-full max-w-xs">
-                    <div className="label">
-                        <span className="label-text">Jumlah Produk*(Wajib)</span>
-                    </div>
-                    <input type="text" placeholder="Jumlah" className="input input-bordered w-full max-w-xs" />
                 </label>
             </div>
             <button className="btn bg-blue-900 my-5 text-white">Simpan</button>

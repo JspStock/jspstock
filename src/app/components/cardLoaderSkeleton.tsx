760f2529e03392e452005e4779f2185dd9c84a0e
Eugene702 <eugenefeilianputrarangga@gmail.com>
2024-04-17 23:52:55 +0700

    Feat: Membetulkan skeleton loading untuk laporan ringkasan

--- src/app/components/cardLoaderSkeleton.tsx
+++
@@ -1,2 +0,0 @@
-const CardLoaderSkeleton = () => <div className="skeleton w-full h-32"></div>
-export default CardLoaderSkeleton
--- src/app/components/reports/ringkasan/loader.tsx
+++ src/app/components/reports/ringkasan/loader.tsx
@@ -1,10 +1,9 @@
 import dynamic from "next/dynamic";
 
-const CardLoder = dynamic(() => import('@/app/components/cardLoaderSkeleton'))
 const Loader = () => {
     return <div className="grid lg:grid-cols-3 md:grid-cols-2 max-md:grid-cols-1 gap-5">
         {
-            Array.from({ length: 11 }).map((_, index) => <div className="card" key={index}>
+            Array.from({ length: 11 }).map((_, index) => <div className="card bg-white" key={index}>
                 <div className="card-body">
                 <div className="skeleton h-4 w-full"></div>
                 <div className="skeleton h-4 w-2/4"></div>

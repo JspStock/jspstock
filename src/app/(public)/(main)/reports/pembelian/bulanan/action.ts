f954e73a635f6dc21adfe8ddaa72734fa133d0ac
Eugene702 <eugenefeilianputrarangga@gmail.com>
2024-04-19 04:29:43 +0700

    Feat: Menyelesaikan laporan pembelian

---
+++ src/app/(public)/(main)/reports/pembelian/bulanan/action.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { cookies } from "next/headers"
+import prisma from "../../../../../../../prisma/database"
+
+const today = new Date()
+export const getPurchase = async ({ year }: { year?: number }) => {
+    return await prisma.purchase.findMany({
+        where: {
+            idStore: cookies().get('store')?.value,
+            createdAt: {
+                gte: year != undefined ? new Date(year, 1, 0) : new Date(today.getFullYear(), 1, 0),
+                lte: year != undefined ? new Date(year, 12, 0) : new Date(today.getFullYear(), 12, 0)
+            }
+        },
+        select: {
+            id: true,
+            shippingCost: true,
+            discount: true,
+            purchaseOrder: {
+                select: {
+                    qty: true,
+                    product: {
+                        select: {
+                            price: true
+                        }
+                    }
+                }
+            },
+            createdAt: true
+        }
+    })
+}
---
+++ src/app/(public)/(main)/reports/pembelian/harian/action.ts
@@ -0,0 +1,33 @@
+"use server"
+
+import { cookies } from "next/headers"
+import prisma from "../../../../../../../prisma/database"
+
+const today = new Date()
+export const getPurchase = async ({ year, month }: { year?: number, month?: number }) => {
+    return await prisma.purchase.findMany({
+        where: {
+            idStore: cookies().get('store')?.value,
+            createdAt: {
+                gte: year != undefined && month != undefined ? new Date(year, month - 1, 1) : new Date(today.getFullYear(), today.getMonth(), 1),
+                lte: year != undefined && month != undefined ? new Date(year, month, 0) : new Date(today.getFullYear(), today.getMonth() + 1, 0)
+            }
+        },
+        select: {
+            id: true,
+            shippingCost: true,
+            discount: true,
+            purchaseOrder: {
+                select: {
+                    qty: true,
+                    product: {
+                        select: {
+                            price: true
+                        }
+                    }
+                }
+            },
+            createdAt: true
+        }
+    })
+}
